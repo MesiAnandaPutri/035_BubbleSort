5d5e9f1d48f3ab38e1deaf903c232f64fa414def
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-07 20:00:56 +0700

    menambahkan fungsi input

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -6,3 +6,32 @@ using namespace std;
 
 int arr[20];                // Deklarasi variable global array a dengan ukuran 20
 int n;                     // Deklarasi variable n untuk menyimpan banyaknhya elemen pada array
+
+void input() {
+
+	while (true) {
+		cout << "Masukan banyaknya elemen pada array : ";
+		cin >> n;
+		if (n <= 20)    //jika n kurang dari atau sama dengan 20
+			break;      //keluar dari loop
+		else {          //jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";  //output ke layar
+		}
+	}
+
+	cout << endl;                                 //output baris kosong
+	cout << "=======================" << endl;              //output ke layar
+	cout << "Masukan Elemen Array" << endl;       //output ke layar
+	cout << "=======================" << endl;    //output ke layar
+
+	for (int i = 0; i < n; i++) {         //looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";   //output ke layar
+		cin >> arr[i];
+	}
+
+}
+
+
+
+
+
