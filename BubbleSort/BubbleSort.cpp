264908a32970b7acb7f8735fa44e651542aad3b4
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-07 20:02:51 +0700

    menambahkan fungsi bubble sort  array

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -31,6 +31,29 @@ void input() {
 
 }
 
+void BubbleShortArray() { // procedur untuk mengurutkan array dengan metode Bubble Sort
+	int pass = 1;   //step 1
+
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) {    //step 2
+			if (arr[j] > arr[j + 1]) {               //step 3
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+		pass = pass + 1;    //step 4
+
+		cout << "\nPass " << -1 << ": "; // output ke layar
+		for (int k = 0; k < n; k++) {    // looping nilai k dimulai dari 0 hingga n-1
+			cout << arr[k] << " ";       // output ke layar
+
+		}
+		cout << endl;
+
+	} while (pass <= n - 1);    // step 5
+}
+
 
 
 
