bd72a433349f4aada20e783d8b6dcfd80a7bc36f
MesiAnandaPutri <mesianandaputri24@gmail.com>
2024-03-07 20:06:16 +0700

    menambahkan program utama dan memanggil fungsi kedalam program utama

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -73,6 +73,14 @@ void display() {
 
 }
 
+int main() {
+	input(); // call the input function to get user input
+	BubbleShortArray();  //call the selection sort function
+	display();   // display the sorted array
+	system("pause");
+
+	return 0;
+}
 
 
 
